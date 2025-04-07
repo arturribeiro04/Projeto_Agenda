0e871d70c188655be5b0222b2dd6f0dd0d3d6aec
Artur Ribeiro <88896435+arturribeiro04@users.noreply.github.com>
2025-04-07 09:17:28 -0300

    Alteração 2 - 07/04
    - Apresentação do conceito de tupla
    - Apresentação do conceito de dicionário

---
+++ arquivos/dicionario.py
@@ -0,0 +1,16 @@
+idade_aluno = {
+    
+    "Maria": 19,
+    "Joao" : 23,
+    "Lucas" : 21,
+    "Roberta" : 22,
+
+}
+
+print(idade_aluno["Roberta"])
+
+for aluno in idade_aluno:
+    print(aluno)
+
+for idade in idade_aluno.values():
+    print(idade)
---
+++ arquivos/tupla.py
@@ -0,0 +1,8 @@
+tupla_cores = ("amarelo", "azul", "vermelho")
+lista_cores = ["roxo","rosa","branco"]
+
+print(lista_cores)
+print(tupla_cores)
+
+for cor in tupla_cores:
+    print(cor)
