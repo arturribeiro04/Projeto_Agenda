9ee55ead5048a30e08ade00df6e457594e4e73a6
Artur Ribeiro <88896435+arturribeiro04@users.noreply.github.com>
2025-04-09 15:49:52 -0300

    Alteração 4 - 09/04
    - Definições de SCRUM para o projeto e aplicação da ferramenta Trello
    - Definições de CRUD para o projeto
    - Definição de métodos e funções

---
+++ arquivos/agenda.py
@@ -0,0 +1,8 @@
+AGENDA = {}
+
+
+def primeiro_metodo():
+    print("PRIMEIRO MÉTODO")
+    pass
+
+primeiro_metodo()
---
+++ arquivos/soma.py
@@ -0,0 +1,7 @@
+parcela_um = 2
+parcela_dois = 3
+
+def soma(parcela_um, parcela_dois):
+    print("TOTAL: ",(parcela_um + parcela_dois))
+
+soma(parcela_um,parcela_dois)
