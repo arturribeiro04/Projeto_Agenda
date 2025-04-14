57efa38a93a2f3293e156704823be777e0e869ea
Artur Ribeiro <88896435+arturribeiro04@users.noreply.github.com>
2025-04-14 17:30:53 -0300

    Altreação 5 - 14/04
    - Criação dos métodos de mostrar toda a agenda e de mostrar contato na agenda

--- arquivos/agenda.py
+++ arquivos/agenda.py
@@ -1,8 +1,41 @@
 AGENDA = {}
 
+AGENDA['guilherme'] = {
 
-def primeiro_metodo():
-    print("PRIMEIRO MÉTODO")
-    pass
+    'telefone' : '91122-3344',
+    'email' : 'guilherme@email.com.br',
+    'endereco' : 'Rua do Guilherme, 23',
 
-primeiro_metodo()
+}
+
+AGENDA['maria'] = {
+
+    'telefone' : '92233-4455',
+    'email' : 'maria@email.com.br',
+    'endereco' : 'Avenida da Maria, 1023',
+
+}
+
+def mostrar_contatos():
+
+    for contato in AGENDA:
+
+        print("NOME: ", contato)
+        print("TELEFONE: ", AGENDA[contato]['telefone'])
+        print("ENDEREÇO: ", AGENDA[contato]['endereco'])
+        print("E-MAIL: ", AGENDA[contato]['email'])
+        print("-" * 120)
+
+def buscar_contato(contato):
+   
+    print("NOME: ", contato)
+    print("TELEFONE: ", AGENDA[contato]['telefone'])
+    print("ENDEREÇO: ", AGENDA[contato]['endereco'])
+    print("E-MAIL: ", AGENDA[contato]['email'])
+    print("-" * 120)
+
+
+print("-" * 20 + "MOSTRAR TODOS OS CONTATOS" + "-" * 20)
+
+mostrar_contatos()
+buscar_contato('maria')
